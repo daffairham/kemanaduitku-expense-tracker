44e10023ecba534d5347905386d7043eb7688e97
daffairham <dafirham@gmail.com>
2026-06-17 13:26:12 +0700

    add env variable to backend port and api.js

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
   res.send("API jalan");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server jalan di PORT ${PORT}`);
