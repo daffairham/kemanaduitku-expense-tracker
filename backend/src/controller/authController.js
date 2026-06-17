a455b4ae1e01eb27995eb56917ca98125ea927f0
daffairham <dafirham@gmail.com>
2026-06-17 13:35:10 +0700

    Update authController.js

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -48,7 +48,7 @@ const userLogin = async (req, res) => {
   });
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
     sameSite: "strict",
     maxAge: 24 * 60 * 60 * 1000,
   });
